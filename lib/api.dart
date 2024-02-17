a8156f82dc737f9d06b114ce611bc67a031a541b
stobertonf <stobertonf@gmail.com>
2024-02-17 16:05:17 -0300

    feat(Video-List): Creating the Api File.

---
+++ lib/api.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_tube/models/video_model.dart';
+
+//TODO: Colocar a chave da API.
+const API_KEY = "";
+
+class Api {
+  late String _search;
+  late String _nextToken;
+
+  Future<List<Video>> search(String search) async {
+    _search = search;
+
+    http.Response response = await http.get(
+        "https://www.googleapis.com/youtube/v3/search?part=snippet&q=$search&type=video&key=$API_KEY&maxResults=10"
+            as Uri);
+
+    return decode(response);
+  }
+
+  Future<List<Video>> nextPage() async {
+    http.Response response = await http.get(
+        "https://www.googleapis.com/youtube/v3/search?part=snippet&q=$_search&type=video&key=$API_KEY&maxResults=10&pageToken=$_nextToken"
+            as Uri);
+
+    return decode(response);
+  }
+
+  List<Video> decode(http.Response response) {
+    if (response.statusCode == 200) {
+      var decoded = json.decode(response.body);
+
+      _nextToken = decoded["nextPageToken"];
+
+      List<Video> videos = decoded["items"].map<Video>((map) {
+        return Video.fromJson(map);
+      }).toList();
+
+      return videos;
+    } else {
+      throw Exception(
+        "Failed to load videos",
+      );
+    }
+  }
+}
