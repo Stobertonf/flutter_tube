b4760d8e00632beaefd79dbc4c9b126cee3b97c6
stobertonf <stobertonf@gmail.com>
2024-02-18 16:11:48 -0300

    feat(Home-Page): Changing the Videos Block file.

--- lib/blocs/videos_bloc.dart
+++ lib/blocs/videos_bloc.dart
@@ -2,28 +2,41 @@ import 'dart:async';
 import 'package:flutter_tube/api.dart';
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter_tube/models/video_model.dart';
+import 'package:flutter/foundation.dart';
 
-abstract class VideosBloc implements BlocBase {
-  late Api api = Api();
+class VideosBloc with ChangeNotifier implements BlocBase {
+  late Api api;
   List<Video> videos = [];
+
   Sink<String> get inSearch => _searchController.sink;
   Stream<List<Video>> get outVideos => _videosController.stream;
   final StreamController<List<Video>> _videosController =
       StreamController<List<Video>>.broadcast();
   final StreamController<String> _searchController = StreamController<String>();
 
-  VideosBloc() {
+  VideosBloc(this.api) {
     _searchController.stream.listen(_search);
   }
 
   void _search(String search) async {
     List<Video> searchedVideos = await api.search(search);
+    videos = searchedVideos;
     _videosController.sink.add(searchedVideos);
+    notifyListeners();
   }
 
   @override
   void dispose() {
+    super.dispose();
     _videosController.close();
     _searchController.close();
+  }
+
+  void search(String query) {
+    inSearch.add(query);
+  }
+
+  Stream<List<Video>> getVideos() {
+    return outVideos;
   }
 }
